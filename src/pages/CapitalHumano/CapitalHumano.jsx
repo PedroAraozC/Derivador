c908efb673258d57b3ad8671ee66512ae3fd7d2b
guille-alonso <guilloalonsot@gmail.com>
2024-01-26 12:50:15 -0300

    bug spinner listo

--- src/pages/CapitalHumano/CapitalHumano.jsx
+++ src/pages/CapitalHumano/CapitalHumano.jsx
@@ -5,21 +5,23 @@ import GraficosCapHumanoPlantaPorReparticion from "../../components/Graficos/Gra
 import TablaCapitalHumano from "../../components/Tablas/TablaCapitalHumano";
 import useStore from "../../Zustand/Zustand";
 import "./CapitalHumano.css";
+import { Box, CircularProgress } from "@mui/material";
 
 const CapitalHumano = () => {
-  const { resultSearch, valuesCapHumano, setResultSearch } = useStore();
+  const { resultSearch, valuesCapHumano, setResultSearch, setValuesCapHumano } = useStore();
 
   useEffect(() => {
     setResultSearch([]);
+    setValuesCapHumano("");
   }, []);
 
   return (
     <>
       <div className="d-flex flex-column justify-content-center align-items-center container mt-5">
         <FormCapitalHumano />
-        {resultSearch[0]?.length > 0 ? <TablaCapitalHumano /> : <></>}
+        {resultSearch[0]?.length !== 0 && <TablaCapitalHumano /> }
       </div>
-      {resultSearch[0]?.length !== 0 && (
+      {resultSearch[0]?.length !== 0 ? (
         <div className="container containerGrafico mb-3">
           {resultSearch[0]?.length > 0 &&
           valuesCapHumano.includes("municipal") ? (
@@ -31,7 +33,11 @@ const CapitalHumano = () => {
             <></>
           )}
         </div>
-      )}
+      ): valuesCapHumano != "" &&
+       <Box className="d-flex justify-content-center">
+        <CircularProgress />
+       </Box>
+    }
     </>
   );
 };
