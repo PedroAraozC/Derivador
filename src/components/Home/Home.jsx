c90fdfdc5fae61045139134aebf3ce978cdfc72e
PedroAraozC <pedroaraozc@gmail.com>
2024-07-03 09:38:09 -0300

    //

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,9 +4,12 @@ import {
   faNewspaper,
   // faFolderOpen,
 } from "@fortawesome/free-regular-svg-icons";
-import { 
+import {
   // faBuildingCircleCheck,
-   faCommentsDollar, faNotesMedical, faQrcode } from "@fortawesome/free-solid-svg-icons";
+  faCommentsDollar,
+  faNotesMedical,
+  faQrcode,
+} from "@fortawesome/free-solid-svg-icons";
 import Card from "../Card/Card";
 import "./Home.css";
 import useStore from "../../Zustand/Zustand";
@@ -63,61 +66,64 @@ const Home = () => {
   };
 
   const irACARNETSANIDAD = () => {
-
     const url = new URL(
-      `http://181.105.6.205:82/cs/index_cd.php?dni=${cuilToDni(user.documento_persona.toString())}`
+      `http://181.105.6.205:82/cs/index_cd.php?dni=${cuilToDni(
+        user.documento_persona.toString()
+      )}`
     );
-    
+
     window.open(url.toString(), "_blank");
-  }
+  };
 
   return (
     <div className="contPadreHome">
-      <div className="cardsContHome">
-        <Card
-          onClick={() => irABOLETIN()}
-          titulo={"Boletin Oficial"}
-          descripcion={
-            "Publicación Digital que contiene la normativa municipal y actos de gobierno"
-          }
-          Icono={<FontAwesomeIcon icon={faNewspaper} />}
-        />
-        <Card
-          onClick={() => irACREDENCIAL()}
-          titulo={"Credencial"}
-          descripcion={"Ver credencial digital"}
-          Icono={<FontAwesomeIcon icon={faQrcode} />}
-        />
-        {/* <Card
+        <div className="cardsContHome">
+          <Card
+            onClick={() => irABOLETIN()}
+            titulo={"Boletin Oficial"}
+            descripcion={
+              "Publicación Digital que contiene la normativa municipal y actos de gobierno"
+            }
+            Icono={<FontAwesomeIcon icon={faNewspaper} />}
+          />
+          <Card
+            onClick={() => irACREDENCIAL()}
+            titulo={"Credencial"}
+            descripcion={"Ver credencial digital"}
+            Icono={<FontAwesomeIcon icon={faQrcode} />}
+          />
+          {/* <Card
           onClick={() => irAEXPEDIENTES()}
           titulo={"Expedientes"}
           descripcion={"Ingreso a sistema de expedientes"}
           Icono={<FontAwesomeIcon icon={faFolderOpen} />}
         /> */}
-        <Card
-          onClick={() => irATURNOS()}
-          titulo={"Licencia de Conducir"}
-          descripcion={"Requsitos para Licencia de conducir"}
-          Icono={<FontAwesomeIcon icon={faIdCard} />}
-        />
-        {/* <Card
+          <Card
+            onClick={() => irATURNOS()}
+            titulo={"Licencia de Conducir"}
+            descripcion={"Requsitos para Licencia de conducir"}
+            Icono={<FontAwesomeIcon icon={faIdCard} />}
+          />
+          {/* <Card
           onClick={() => irACATASTRO()}
           titulo={"Catastro"}
           descripcion={"Catastro y Edificaciones"}
           Icono={<FontAwesomeIcon icon={faBuildingCircleCheck} />}
         /> */}
-        <Card
-          onClick={() => irALICITACIONES()}
-          titulo={"Licitaciones"}
-          descripcion={"Convocatoria para la contratación de bienes, obras y servicios."}
-          Icono={<FontAwesomeIcon icon={faCommentsDollar} />}
-        />
-        <Card
-          onClick={() => irACARNETSANIDAD()}
-          titulo={"Carnet de Sanidad"}
-          descripcion={"Consulta del Carnet Digital"}
-          Icono={<FontAwesomeIcon icon={faNotesMedical}/>}
-        />
+          <Card
+            onClick={() => irALICITACIONES()}
+            titulo={"Licitaciones"}
+            descripcion={
+              "Convocatoria para la contratación de bienes, obras y servicios."
+            }
+            Icono={<FontAwesomeIcon icon={faCommentsDollar} />}
+          />
+          <Card
+            onClick={() => irACARNETSANIDAD()}
+            titulo={"Carnet de Sanidad"}
+            descripcion={"Consulta del Carnet Digital"}
+            Icono={<FontAwesomeIcon icon={faNotesMedical} />}
+          />
       </div>
     </div>
   );
