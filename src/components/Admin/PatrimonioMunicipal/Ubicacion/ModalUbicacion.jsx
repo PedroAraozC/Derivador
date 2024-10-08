795cddb0ad143505f9172d1d2b3750c974b85ae0
PedroAraozC <pedroaraozc@gmail.com>
2024-10-08 11:28:49 -0300

    se modificaron funciones de edicion de materia, estado, autor, etc de patrimonio

--- src/components/Admin/PatrimonioMunicipal/Ubicacion/ModalUbicacion.jsx
+++ src/components/Admin/PatrimonioMunicipal/Ubicacion/ModalUbicacion.jsx
@@ -1,31 +1,46 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
 import { useState, useEffect, useContext } from "react";
-import { Modal, Box, Button, Divider, InputLabel, Switch, TextField, Snackbar, Alert } from "@mui/material";
+import {
+  Modal,
+  Box,
+  Button,
+  Divider,
+  InputLabel,
+  Switch,
+  TextField,
+  Snackbar,
+  Alert,
+} from "@mui/material";
 import { EducaContext } from "../../../../context/EducaContext";
 import axios from "../../../../config/axios";
+import axiosLici from "../../../../config/axiosLicitaciones";
 
-const ModalUbicacion = ({ubicaciones, modalAbierto, handleClose}) => {
-
+const ModalUbicacion = ({ ubicaciones, modalAbierto, handleClose }) => {
   const [deviceWidth, setDeviceWidth] = useState(window.innerWidth);
   const { actualizador } = useContext(EducaContext);
   const [errores, setErrores] = useState({});
   const [snackbarOpen, setSnackbarOpen] = useState(false);
-  const [snackbarMensaje, setSnackbarMensaje] = useState('');
+  const [snackbarMensaje, setSnackbarMensaje] = useState("");
   const [formularioValues, setFormularioValues] = useState({
-    nombre_estado: "", 
-    habilita: 0, 
- });
- const handleSnackbarClose = () => {
-  setSnackbarOpen(false);
-};
+    nombre_ubicacion: "",
+    habilita: 0,
+  });
+  const handleSnackbarClose = () => {
+    setSnackbarOpen(false);
+  };
   // Función para validar el formulario antes de enviarlo
   const validarFormulario = () => {
     const nuevosErrores = {};
 
-    if (!formularioValues.nombre_estado || formularioValues.nombre_estado.length > 30) {
-      nuevosErrores.nombre_estado = "Ingrese un nombre de máximo 20 caracteres";
-      setSnackbarMensaje("Ingrese un nombre del autor/a de máximo 20 caracteres");
+    if (
+      !formularioValues.nombre_ubicacion ||
+      formularioValues.nombre_ubicacion.length > 30
+    ) {
+      nuevosErrores.nombre_ubicacion = "Ingrese un nombre de máximo 20 caracteres";
+      setSnackbarMensaje(
+        "Ingrese un nombre del autor/a de máximo 20 caracteres"
+      );
     }
 
     setErrores(nuevosErrores);
@@ -37,7 +52,6 @@ const ModalUbicacion = ({ubicaciones, modalAbierto, handleClose}) => {
 
     return Object.keys(nuevosErrores).length === 0; // Retorna true si no hay errores
   };
-
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -60,7 +74,7 @@ const ModalUbicacion = ({ubicaciones, modalAbierto, handleClose}) => {
     if (ubicaciones) {
       // Si hay un estado y el modal está abierto, establece los valores del formulario
       setFormularioValues({
-        id: ubicaciones.id_estado,
+        id: ubicaciones.id_ubicacion,
         nombre_ubicacion: ubicaciones.nombre_ubicacion,
         habilita: ubicaciones.habilita,
       });
@@ -81,28 +95,30 @@ const ModalUbicacion = ({ubicaciones, modalAbierto, handleClose}) => {
     return null;
   }
 
-
-  const editarUbicacion = async (event, ubi) => {
-    event.preventDefault()
+  const editarUbicacion = async (event) => {
     const formularioValido = validarFormulario();
+    console.log(formularioValues, "aa")
     if (formularioValido) {
       try {
-        const response = await axios.post(
-          "/admin/editarUbicacion",
-          ubi
+        const response = await axiosLici.post(
+          "/admin/editarUbicacionPatrimonio",
+          formularioValues
         );
-        handleClose()
-        actualizador()
+        // handleClose();
+        actualizador();
+        setSnackbarMensaje("Ubicación editada con éxito.");
+        setSnackbarOpen(true);
         return response.data;
       } catch (error) {
         console.error("Error al editar la ubicacion:", error);
         throw new Error("Error al editar la ubicacion");
       }
     } else {
-      console.log('Algo salio mal :(')
+      console.log("Algo salio mal :(");
       setSnackbarMensaje("Por favor, corrige los errores en el formulario.");
       setSnackbarOpen(true);
-    }};
+    }
+  };
 
   const style = {
     position: "absolute",
@@ -117,7 +133,6 @@ const ModalUbicacion = ({ubicaciones, modalAbierto, handleClose}) => {
     p: 4,
   };
 
-
   return (
     <Modal open={modalAbierto} onClose={handleClose}>
       <Box sx={style}>
@@ -128,45 +143,56 @@ const ModalUbicacion = ({ubicaciones, modalAbierto, handleClose}) => {
         </div>
         <Divider />
         <div className="d-flex flex-column justify-content-center">
-              <form className="d-flex justify-content-around flex-column">
-                <div className="w-50 d-flex flex-column gap-3 p-2">
-                  <InputLabel>ESTADO</InputLabel>
-                  <TextField
-                    placeholder={`${ubicaciones.nombre_ubicacion}`}
-                    onChange={handleInputChange}
-                    name="nombre_ubicacion"
-                    value={formularioValues.nombre_ubicacion}
-                  />
-                </div>
-                <div className="d-flex flex-column gap-3 w-50 p-2">
-                  <div className="d-flex align-items-center">
-                    <p className="m-0">Habilita:</p>
-                    <Switch
-                      checked={formularioValues.habilita === 1}
-                      onChange={handleHabilitarChange}
-                      name="habilita"
-                    />
-                  </div>
-                </div>
-              </form>
-                <Button
-                  onClick={() => editarUbicacion(formularioValues)}
-                  className="mt-3"
-                  variant="outlined"
-                >
-                  Guardar cambios
-                </Button>
+          <form className="d-flex justify-content-around flex-column">
+            <div className="w-50 d-flex flex-column gap-3 p-2">
+              <InputLabel>DIRECCIÓN</InputLabel>
+              <TextField
+                placeholder={`${ubicaciones.nombre_ubicacion}`}
+                onChange={handleInputChange}
+                name="nombre_ubicacion"
+                value={formularioValues.nombre_ubicacion}
+              />
+            </div>
+            <div className="d-flex flex-column gap-3 w-50 p-2">
+              <div className="d-flex align-items-center">
+                <p className="m-0">Habilita:</p>
+                <Switch
+                  checked={formularioValues.habilita === 1}
+                  onChange={handleHabilitarChange}
+                  name="habilita"
+                />
+              </div>
+            </div>
+          </form>
+          <Button
+            onClick={() => editarUbicacion()}
+            className="mt-3"
+            variant="outlined"
+          >
+            Guardar cambios
+          </Button>
         </div>
-        {errores? (
-            <Snackbar open={snackbarOpen} autoHideDuration={6000} onClose={handleSnackbarClose}>
-                <Alert onClose={handleSnackbarClose} severity="info" elevation={6} variant="filled">
-                    {snackbarMensaje}
-                </Alert>
-            </Snackbar>
-            ):<></>}
+        {errores ? (
+          <Snackbar
+            open={snackbarOpen}
+            autoHideDuration={6000}
+            onClose={handleSnackbarClose}
+          >
+            <Alert
+              onClose={handleSnackbarClose}
+              severity="info"
+              elevation={6}
+              variant="filled"
+            >
+              {snackbarMensaje}
+            </Alert>
+          </Snackbar>
+        ) : (
+          <></>
+        )}
       </Box>
     </Modal>
-  )
-}
+  );
+};
 
-export default ModalUbicacion
+export default ModalUbicacion;
