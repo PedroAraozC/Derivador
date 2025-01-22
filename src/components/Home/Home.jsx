decd93762387fc4b417443db899b597b227875f4
Guille <guilloalonsot@gmail.com>
2025-01-22 11:10:36 -0300

    home listo

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -38,10 +38,10 @@ const Home = () => {
     window.open(url.toString(), "_blank");
   };
 
-  const irATURNOS = () => {
+  const irATURNOS = (reparticion) => {
     const token = localStorage.getItem("token");
     const url = new URL(
-      `https://turnos.smt.gob.ar/?auth=${token}&destino=turnero&rep=1711`
+      `https://turnos.smt.gob.ar/?auth=${token}&destino=turnero&rep=${reparticion}`
     );
     url.searchParams.append("auth", token);
     window.open(url.toString(), "_blank");
@@ -141,9 +141,15 @@ const Home = () => {
           Icono={<FontAwesomeIcon icon={faFolderOpen} />}
         />
         <Card
-          onClick={() => irATURNOS()}
+          onClick={() => irATURNOS(1711)}
           titulo={"Licencia de Conducir"}
           descripcion={"Requsitos para Licencia de conducir"}
+          Icono={<FontAwesomeIcon icon={faIdCard} />}
+        />
+        <Card
+          onClick={() => irATURNOS(241)}
+          titulo={"Ficha Médica Escolar"}
+          descripcion={"Turnos para obtención de ficha médica en inicio escolar"}
           Icono={<FontAwesomeIcon icon={faIdCard} />}
         />
         <Card
@@ -160,12 +166,14 @@ const Home = () => {
           descripcion={"Consulta de Multas por Dominio"}
           Icono={<FontAwesomeIcon icon={faCar} />}
         />
-        <Card
+
+
+        {/* <Card
           onClick={() => irACEMA()}
           titulo={"Servicios de Población Animal"}
           descripcion={"Turnos y Requsitos"}
           Icono={<FontAwesomeIcon icon={faCat} />}
-        />
+        /> */}
         
         {user.id_tusuario == 1 &&
           <Card
