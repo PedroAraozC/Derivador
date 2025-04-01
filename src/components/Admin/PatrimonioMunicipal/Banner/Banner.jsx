a2957eefdf54c5acb08a64a99d054f306750aad9
Mariano <nanomartinezbm@gmail.com>
2025-04-01 10:44:10 -0300

    test

--- src/components/Admin/PatrimonioMunicipal/Banner/Banner.jsx
+++ src/components/Admin/PatrimonioMunicipal/Banner/Banner.jsx
@@ -4,11 +4,11 @@ const Banner = () => {
   return (
     <div className="container d-flex flex-column justify-content-center align-items-center mb-3">
             <h4 className="mt-3 mb-1">
-              Cambiar Imagenes del Banner
+              Cambiar Las Imagenes del Banner
             </h4>
             <TablaBanner/>
           </div>
   )
 }
 
-export default Banner
+export default Banner 
