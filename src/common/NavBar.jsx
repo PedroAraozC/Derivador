13b395c3ac4e0b9cb719332b181573457a89da86
Guille <guilloalonsot@gmail.com>
2025-07-24 18:51:31 -0300

    sin editar perfil todavia

--- src/common/NavBar.jsx
+++ src/common/NavBar.jsx
@@ -84,7 +84,7 @@ export default function NavBar() {
                       open={Boolean(anchorEl)}
                       onClose={handleClose}
                     >
-                      <MenuItem onClick={goToPerfil}>Mi perfil</MenuItem>
+                      {/* <MenuItem onClick={goToPerfil}>Mi perfil</MenuItem> */}
                       <MenuItem onClick={handleLogout}>Cerrar Sesión</MenuItem>
                     </Menu>
                   </div>
