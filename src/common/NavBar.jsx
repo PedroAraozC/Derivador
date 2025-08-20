00142153768dc97f787f2cdb71823de3ddc2b3e1
Guille <guilloalonsot@gmail.com>
2025-08-20 13:48:52 -0300

    habilitando perfil

--- src/common/NavBar.jsx
+++ src/common/NavBar.jsx
@@ -29,6 +29,7 @@ export default function NavBar() {
     setAnchorEl(null);
 
     const url = new URL(`https://perfil.smt.gob.ar/`);
+    //const url = new URL(`http://localhost:5175/`);
     url.searchParams.append("auth", localStorage.getItem("token"));
     url.searchParams.append("origin", "cidituc");
     window.location.href = url.toString();
@@ -84,7 +85,7 @@ export default function NavBar() {
                       open={Boolean(anchorEl)}
                       onClose={handleClose}
                     >
-                      {/* <MenuItem onClick={goToPerfil}>Mi perfil</MenuItem> */}
+                      <MenuItem onClick={goToPerfil}>Mi perfil</MenuItem>
                       <MenuItem onClick={handleLogout}>Cerrar Sesión</MenuItem>
                     </Menu>
                   </div>
