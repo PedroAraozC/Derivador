ffa7757af7cffffbd1fae86c3f63778638d5ca65
Guille <guilloalonsot@gmail.com>
2026-03-18 11:41:43 -0300

    valdiar usuarios ruta lista

--- src/routes/PrivateRouteUsuarios.jsx
+++ src/routes/PrivateRouteUsuarios.jsx
@@ -5,8 +5,8 @@ import { Box, CircularProgress } from "@mui/material";
 
 // eslint-disable-next-line react/prop-types
 const PrivateRouteUsuarios = ({ children }) => {
-  const { getAuth, authenticated, loading, user } = useStore();
-  
+  const { getAuth, authenticated, loading, user, permisos } = useStore();
+  console.log(permisos.filter(p=>p.id_proceso === 81 && p.ver === 1 ));
   useEffect(() => {
     getAuth();
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -16,7 +16,7 @@ const PrivateRouteUsuarios = ({ children }) => {
     <Box sx={{ display: "flex" }}>
       <CircularProgress />
     </Box>
-  ) : authenticated && (user.id_tusuario == 1 || user.id_tusuario == 40) ? (
+  ) : authenticated && ((user.id_tusuario == 1 || user.id_tusuario == 40) || (permisos.filter(p=>p.id_proceso === 85 && p.ver === 1 ).length > 0)) ? (
     children
   ) : (
     <Navigate to="/login" />
