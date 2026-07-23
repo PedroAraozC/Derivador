ac18b1844a6c9bcc04268257c682cb2abf46c58b
Pedro Aráoz <pedroaraozc@gmail.com>
2026-07-23 11:35:56 -0300

    se unificaron los libre deuda

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ import Reclamos from "./pages/EstadisticasReclamos/Reclamos";
 import PrivateRoute from "./routes/PrivateRoute";
 import PanelAdmin from "./components/Admin/General/PanelAdmin";
 import PanelContratacion from "./components/Admin/Contratacion/PanelContratacion";
-import PanelPatrimonioMunicipal from "./components/Admin/PatrimonioMunicipal/PanelPatrimonioMunicipal"
-import AgregarAutor from "./components/Admin/PatrimonioMunicipal/Autor/AgregarAutor"
+import PanelPatrimonioMunicipal from "./components/Admin/PatrimonioMunicipal/PanelPatrimonioMunicipal";
+import AgregarAutor from "./components/Admin/PatrimonioMunicipal/Autor/AgregarAutor";
 import AgregarPatrimonio from "./components/Admin/PatrimonioMunicipal/AgregarPatrimonio";
 import AgregarEstado from "./components/Admin/PatrimonioMunicipal/Estado/AgregarEstado";
 import AgregarMaterial from "./components/Admin/PatrimonioMunicipal/Material/AgregarMaterial";
@@ -41,16 +41,20 @@ import EstadisticasPersonales from "./pages/DashboardSistemas/EstadisticasPerson
 import ConsultaPublica from "./pages/ConsultaPublica/ConsultaPublica";
 import VerReclamo from "./pages/ConsultaPublica/VerReclamo";
 import AgregarReclamo from "./pages/ConsultaPublica/NuevoReclamo/AgregarReclamo";
+import LibreDeuda from "./pages/LibreDeuda/LibreDeuda";
+import LibreDeudaDIM from "./pages/LibreDeuda/LibreDeudaDIM";
 
 function App() {
   const url = new URL(window.location.href);
   const logout = url.searchParams.get("logout");
   const token = url.searchParams.get("auth");
-  
 
-  if(localStorage.getItem("token")){
-    localStorage.setItem("token", token != null ? token : localStorage.getItem("token"));
-  }else if(token){
+  if (localStorage.getItem("token")) {
+    localStorage.setItem(
+      "token",
+      token != null ? token : localStorage.getItem("token"),
+    );
+  } else if (token) {
     localStorage.setItem("token", token);
   }
 
@@ -67,64 +71,370 @@ function App() {
         <Layout>
           <ProviderDerivador>
             <Routes>
-              <Route exact path="/*" element={<PrivateRoute key="home"><Home /></PrivateRoute>} />
-              <Route exact path="/estadistica_rrhh" element={<PrivateRoute key="cap-humano"><CapitalHumano /></PrivateRoute>}/>
-              <Route exact path="/estadistica_ac" element={<PrivateRoute key="reclamos"><Reclamos /></PrivateRoute>}/>
-
+              <Route
+                exact
+                path="/*"
+                element={
+                  <PrivateRoute key="home">
+                    <Home />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/estadistica_rrhh"
+                element={
+                  <PrivateRoute key="cap-humano">
+                    <CapitalHumano />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/estadistica_ac"
+                element={
+                  <PrivateRoute key="reclamos">
+                    <Reclamos />
+                  </PrivateRoute>
+                }
+              />
 
               {/* ADMINISTRADOR GENERAL */}
-              <Route exact path="/opcion" element={<PrivateRouteAdmin><PanelAdmin /></PrivateRouteAdmin>} />
-              <Route exact path="/genero" element={<PrivateRouteAdmin><PanelGenero /></PrivateRouteAdmin>} />
-              <Route exact path="/tipo-usuario" element={<PrivateRouteAdmin><PanelTUsuarios /></PrivateRouteAdmin>} />
-              <Route exact path="/tipo_documento" element={<PrivateRouteAdmin><PanelTDocumento /></PrivateRouteAdmin>} />
-              <Route exact path="/reparticion" element={<PrivateRouteAdmin><PanelReparticiones /></PrivateRouteAdmin>} />
-              <Route exact path="/permisos-usuario" element={<PrivateRouteAdmin><PermisosTUsuario /></PrivateRouteAdmin>} />
-              <Route exact path="/panel_usuario" element={<PrivateRouteAdmin><PanelUsuarios /></PrivateRouteAdmin>} />
-              <Route exact path="/dashboard-sistemas" element={<PrivateRouteAdmin><DashboardSistemas /></PrivateRouteAdmin>} />
-              <Route exact path="/estadisticas-personales" element={<PrivateRouteAdmin><EstadisticasPersonales /></PrivateRouteAdmin>} />
-              <Route exact path="/validar_usuarios" element={<PrivateRouteUsuarios key="validar_usuarios"><FormularioBusquedaUsuario /></PrivateRouteUsuarios>} />
+              <Route
+                exact
+                path="/opcion"
+                element={
+                  <PrivateRouteAdmin>
+                    <PanelAdmin />
+                  </PrivateRouteAdmin>
+                }
+              />
+              <Route
+                exact
+                path="/genero"
+                element={
+                  <PrivateRouteAdmin>
+                    <PanelGenero />
+                  </PrivateRouteAdmin>
+                }
+              />
+              <Route
+                exact
+                path="/tipo-usuario"
+                element={
+                  <PrivateRouteAdmin>
+                    <PanelTUsuarios />
+                  </PrivateRouteAdmin>
+                }
+              />
+              <Route
+                exact
+                path="/tipo_documento"
+                element={
+                  <PrivateRouteAdmin>
+                    <PanelTDocumento />
+                  </PrivateRouteAdmin>
+                }
+              />
+              <Route
+                exact
+                path="/reparticion"
+                element={
+                  <PrivateRouteAdmin>
+                    <PanelReparticiones />
+                  </PrivateRouteAdmin>
+                }
+              />
+              <Route
+                exact
+                path="/permisos-usuario"
+                element={
+                  <PrivateRouteAdmin>
+                    <PermisosTUsuario />
+                  </PrivateRouteAdmin>
+                }
+              />
+              <Route
+                exact
+                path="/panel_usuario"
+                element={
+                  <PrivateRouteAdmin>
+                    <PanelUsuarios />
+                  </PrivateRouteAdmin>
+                }
+              />
+              <Route
+                exact
+                path="/dashboard-sistemas"
+                element={
+                  <PrivateRouteAdmin>
+                    <DashboardSistemas />
+                  </PrivateRouteAdmin>
+                }
+              />
+              <Route
+                exact
+                path="/estadisticas-personales"
+                element={
+                  <PrivateRouteAdmin>
+                    <EstadisticasPersonales />
+                  </PrivateRouteAdmin>
+                }
+              />
+              <Route
+                exact
+                path="/validar_usuarios"
+                element={
+                  <PrivateRouteUsuarios key="validar_usuarios">
+                    <FormularioBusquedaUsuario />
+                  </PrivateRouteUsuarios>
+                }
+              />
               {/* ADMINISTRADOR GENERAL */}
-              
+
               {/* LICITACION */}
-              <Route exact path="/panel_contratacion" element={<PrivateRoute><PanelContratacion /></PrivateRoute>} />
+              <Route
+                exact
+                path="/panel_contratacion"
+                element={
+                  <PrivateRoute>
+                    <PanelContratacion />
+                  </PrivateRoute>
+                }
+              />
               {/* LICITACION */}
 
-              {/*PATRIMONIO*/ }
-              <Route exact path="/panel_patrimonio" element={<PrivateRoute><PanelPatrimonioMunicipal /></PrivateRoute>} />
-              <Route exact path="/agregar-patrimonio" element={<PrivateRoute><AgregarPatrimonio /></PrivateRoute>} />
-              <Route exact path="/agregar-autor" element={<PrivateRoute><AgregarAutor /></PrivateRoute>} />
-              <Route exact path="/agregar-estado" element={<PrivateRoute><AgregarEstado /></PrivateRoute>} />
-              <Route exact path="/agregar-material" element={<PrivateRoute><AgregarMaterial /></PrivateRoute>} />
-              <Route exact path="/agregar-tipologia" element={<PrivateRoute><AgregarTipologia /></PrivateRoute>} />
-              <Route exact path="/agregar-categoria" element={<PrivateRoute><AgregarCategoria /></PrivateRoute>} />
-              <Route exact path="/agregar-ubicacion" element={<PrivateRoute><AgregarUbicacion /></PrivateRoute>} />
-              {/*PATRIMONIO*/ }
+              {/*PATRIMONIO*/}
+              <Route
+                exact
+                path="/panel_patrimonio"
+                element={
+                  <PrivateRoute>
+                    <PanelPatrimonioMunicipal />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/agregar-patrimonio"
+                element={
+                  <PrivateRoute>
+                    <AgregarPatrimonio />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/agregar-autor"
+                element={
+                  <PrivateRoute>
+                    <AgregarAutor />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/agregar-estado"
+                element={
+                  <PrivateRoute>
+                    <AgregarEstado />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/agregar-material"
+                element={
+                  <PrivateRoute>
+                    <AgregarMaterial />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/agregar-tipologia"
+                element={
+                  <PrivateRoute>
+                    <AgregarTipologia />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/agregar-categoria"
+                element={
+                  <PrivateRoute>
+                    <AgregarCategoria />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/agregar-ubicacion"
+                element={
+                  <PrivateRoute>
+                    <AgregarUbicacion />
+                  </PrivateRoute>
+                }
+              />
+              {/*PATRIMONIO*/}
 
               {/* BOTON DE PAGO */}
-              <Route exact path="/LibreDeudaPagoExitoso" element={<PrivateRoute key="success"><SuccessPage/></PrivateRoute>} />
-              <Route exact path="/LibreDeudaPagoRechazado" element={<PrivateRoute key="errorPago"><ErrorPage/></PrivateRoute>} />
-              <Route exact path="/montos" element={<PrivateRoute key="montos"><Montos/></PrivateRoute>} />
+              <Route
+                exact
+                path="/LibreDeudaPagoExitoso"
+                element={
+                  <PrivateRoute key="success">
+                    <SuccessPage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/LibreDeudaPagoRechazado"
+                element={
+                  <PrivateRoute key="errorPago">
+                    <ErrorPage />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/montos"
+                element={
+                  <PrivateRoute key="montos">
+                    <Montos />
+                  </PrivateRoute>
+                }
+              />
 
-              <Route exact path="/multas" element={<PrivateRoute key="multas"><Multas/></PrivateRoute>} />
-              
+              <Route
+                exact
+                path="/multas"
+                element={
+                  <PrivateRoute key="multas">
+                    <Multas />
+                  </PrivateRoute>
+                }
+              />
+
               {/* VIA PUBLICA */}
-              <Route exact path="/via-publica" element={<PrivateRoute key="via-publica"><Solicitud_permisos/></PrivateRoute>} />
+              <Route
+                exact
+                path="/via-publica"
+                element={
+                  <PrivateRoute key="via-publica">
+                    <Solicitud_permisos />
+                  </PrivateRoute>
+                }
+              />
 
               {/* CONSULTA PUBLICA */}
-              <Route exact path="/consulta-publica" element={<PrivateRoute key="consulta-publica"><ConsultaPublica /></PrivateRoute>} />
-              <Route exact path="/ver_reclamo/:id" element={<PrivateRoute key="consulta-publica"><VerReclamo /></PrivateRoute>} />
-              <Route exact path="/consulta-publica/agregar-reclamo" element={<PrivateRoute key="agregar-reclamo"><AgregarReclamo /></PrivateRoute>} />
+              <Route
+                exact
+                path="/consulta-publica"
+                element={
+                  <PrivateRoute key="consulta-publica">
+                    <ConsultaPublica />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/ver_reclamo/:id"
+                element={
+                  <PrivateRoute key="consulta-publica">
+                    <VerReclamo />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/consulta-publica/agregar-reclamo"
+                element={
+                  <PrivateRoute key="agregar-reclamo">
+                    <AgregarReclamo />
+                  </PrivateRoute>
+                }
+              />
 
               {/* OBRAS PUBLICAS */}
-              <Route exact path="/el-gas-llega-a-tu-casa" element={<PrivateRoute key="elGasATuCasa"><GasATuCasa /></PrivateRoute>} />
-              <Route exact path="/el-gas-a-tu-casa-backoffice" element={<PrivateRoute key="elGasATuCasaBackOffice"><PanelPersonasGas /></PrivateRoute>} />
+              <Route
+                exact
+                path="/el-gas-llega-a-tu-casa"
+                element={
+                  <PrivateRoute key="elGasATuCasa">
+                    <GasATuCasa />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/el-gas-a-tu-casa-backoffice"
+                element={
+                  <PrivateRoute key="elGasATuCasaBackOffice">
+                    <PanelPersonasGas />
+                  </PrivateRoute>
+                }
+              />
 
               {/* COMBUSTIBLES */}
-              <Route exact path="/importarExcelCombustibles" element={<PrivateRoute key="importarExcel"><CargarExcel /></PrivateRoute>} />
-              <Route exact path="/leerDatosCombustibles" element={<PrivateRoute key="VerConsumosCombustibles"><ConsumosTable /></PrivateRoute>} />
-              <Route exact path="/leerDatosVehiculos" element={<PrivateRoute key="vehiculosCombustibles"><VehiculosTable /></PrivateRoute>} />
-              <Route exact path="/menuCombustibles" element={<PrivateRoute key="menuCombustibles"><MenuCombustibles/></PrivateRoute>} />
+              <Route
+                exact
+                path="/importarExcelCombustibles"
+                element={
+                  <PrivateRoute key="importarExcel">
+                    <CargarExcel />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/leerDatosCombustibles"
+                element={
+                  <PrivateRoute key="VerConsumosCombustibles">
+                    <ConsumosTable />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/leerDatosVehiculos"
+                element={
+                  <PrivateRoute key="vehiculosCombustibles">
+                    <VehiculosTable />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/menuCombustibles"
+                element={
+                  <PrivateRoute key="menuCombustibles">
+                    <MenuCombustibles />
+                  </PrivateRoute>
+                }
+              />
 
+              {/* LIBRE DEUDA */}
+              <Route
+                exact
+                path="/libre-deuda"
+                element={
+                  <PrivateRoute key="libre-deuda">
+                    <LibreDeuda />
+                  </PrivateRoute>
+                }
+              />
+              <Route
+                exact
+                path="/libre-deuda-dim"
+                element={
+                  <PrivateRoute key="libre-deuda-dim">
+                    <LibreDeudaDIM />
+                  </PrivateRoute>
+                }
+              />
             </Routes>
           </ProviderDerivador>
         </Layout>
